b7a8c31b22951dcfb23f27a96a71c1fb99938ef5
Ton <washingtonf.nascimento@gmail.com>
2021-02-02 19:50:09 -0400

    Configuração das models

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -16,7 +16,13 @@ class CreateProductService {
   constructor(private productsRepository: IProductsRepository) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return product;
   }
 }
 
